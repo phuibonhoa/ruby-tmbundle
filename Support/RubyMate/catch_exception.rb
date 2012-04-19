9d47413f438833ab49a13f09ccef51277515f298
Philippe Huibonhoa <phuibonhoa@gmail.com>
2012-04-18 18:25:14 -0700

    Fixing weird issue for ruby 1.9, that would segfault when printing exceptions.  https://gist.github.com/1333721/196a288f84355b9d8b7372d6834d25f5036f7ea5

--- Support/RubyMate/catch_exception.rb
+++ Support/RubyMate/catch_exception.rb
@@ -8,11 +8,10 @@ require 'pathname'
 at_exit do
   if (e = $!) && !e.instance_of?(SystemExit)
     require "#{ENV['TM_SUPPORT_PATH']}/lib/escape"
-    require "cgi"
     io = IO.for_fd(ENV['TM_ERROR_FD'].to_i)
 
     io.write "<div id='exception_report' class='framed'>\n"
-    io.write "<p id='exception'><strong>#{e.class.name}:</strong> #{CGI.escapeHTML e.message.sub(/`(\w+)'/, '‘\1’').sub(/ -- /, ' — ')}</p>\n"
+    io.write "<p id='exception'><strong>#{e.class.name}:</strong> #{htmlize e.message.sub(/`(\w+)'/, '‘\1’').sub(/ -- /, ' — ')}</p>\n"
 
     io.write "<blockquote><table border='0' cellspacing='4' cellpadding='0'>\n"
 
@@ -28,8 +27,8 @@ at_exit do
         end
           
         io << "<tr><td><a class='near' href='txmt://open?line=#{line + url}'>"
-        io << (method ? "method #{CGI::escapeHTML method}" : '<em>at top level</em>')
-        io << "</a></td>\n<td>in <strong>#{CGI::escapeHTML display_name}</strong> at line #{line}</td></tr>\n"
+        io << (method ? "method #{htmlize method}" : '<em>at top level</em>')
+        io << "</a></td>\n<td>in <strong>#{htmlize display_name}</strong> at line #{line}</td></tr>\n"
       end
     end
     
