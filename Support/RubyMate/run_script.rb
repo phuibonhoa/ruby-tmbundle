35c29823000a615ef2bb4cb19c16312c8b0d11cb
Philippe Huibonhoa <phuibonhoa@gmail.com>
2012-11-26 16:00:53 -0800

    Updated run script to only use bundle exec if a Gemfile exists in the run path

--- Support/RubyMate/run_script.rb
+++ Support/RubyMate/run_script.rb
@@ -60,13 +60,13 @@ end
 
 
 class TestResultParser
-  
+
   attr_reader :buffer, :in_buffer, :printed_header
   attr_accessor :test_file
   alias :in_buffer? :in_buffer
   alias :printed_header? :printed_header
   alias :test_file? :test_file
-  
+
   def initialize(attributes={})
     attributes = {:test_file => false}.merge(attributes)
     @buffer = []
@@ -74,7 +74,7 @@ class TestResultParser
     @printed_header = false
     @test_file = attributes.fetch(:test_file)
   end
-  
+
   def header
     <<-EOT
 <link href="file://#{URI.escape ENV['TM_BUNDLE_SUPPORT']}/assets/buttons.css" type="text/css" rel="stylesheet"/>
@@ -84,30 +84,30 @@ class TestResultParser
   }
 
   .red {
-    color:red; 
+    color:red;
     font-weight:bold;
   }
-  
+
   .green {
     color:green;
   }
-  
+
   .puts {
-    color: #141414; 
-    background-color: rgba(0, 0, 0, 0.2); 
-    padding: 2px 9px; 
-    border-bottom-left-radius: 5px; 
-    border-bottom-right-radius: 5px; 
+    color: #141414;
+    background-color: rgba(0, 0, 0, 0.2);
+    padding: 2px 9px;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
     margin-bottom: 5px
   }
-  
+
   .test_name {
     padding: 1px 9px 0 9px;
   }
-  
+
   .puts_header {
-    background-color: rgba(0, 0, 0, 0.2); 
-    border-top-left-radius: 5px; 
+    background-color: rgba(0, 0, 0, 0.2);
+    border-top-left-radius: 5px;
     border-top-right-radius: 5px;
   }
 </style>
@@ -120,41 +120,41 @@ class TestResultParser
 </script>
     EOT
   end
-  
+
   def test_result_line?(line)
     line =~ /^\w+#test[:_]/ || in_buffer?
   end
-  
-  def process_line(line)    
+
+  def process_line(line)
     if line =~ /^\w+#test[:_]/
       @buffer = [line]
       @in_buffer = true
     elsif in_buffer?
       @buffer << line
     end
-    
+
     if line =~ /= [\.EF]$/
       @in_buffer = false
-      
+
       all_lines = buffer.join
       match = all_lines.match(/(.*?(\.  = ))(.*?)([\d\.]+ . = [\.EF])\Z/m)
-      
+
       if match
         test_result = match[1] + match[4]
         test_puts = match[3]
-                
+
       else
         test_result = all_lines
       end
-      
+
       css_class = if line =~ /(E|F)$/
         'red'
       else
         'green'
       end
-      
+
       test_has_puts = !(test_puts.nil? || test_puts.strip == "")
-      
+
       out = "<div class=\"#{css_class} test_name #{'puts_header' if test_has_puts}\">#{test_result}</div>"
       out << "<div class=\"#{css_class} puts\">#{htmlize(test_puts)}</div>" if test_has_puts
       out
@@ -162,7 +162,7 @@ class TestResultParser
       ""
     end
   end
-  
+
   def post_filter(output)
     if printed_header? or !test_file?
       output
@@ -173,31 +173,41 @@ class TestResultParser
   end
 end
 
+def path_includes_gemfile?(path)
+  dir_parts = File.dirname(path).split("/")
+  current_path = "/"
+  dir_parts.each do |part|
+    current_path = File.join(current_path, part)
+    return true if File.exists?(File.join(current_path, "Gemfile"))
+  end
+  false
+end
+
 trp = TestResultParser.new(:test_file => is_test_script)
 
 if ENV['TM_BUNDLE'] || ENV['TM_RUBY'].to_s =~ /\.rvm/
   bundle_cmd = if ENV['TM_BUNDLE']
-    ENV['TM_BUNDLE'] 
-  else
+    ENV['TM_BUNDLE']
+  elsif path_includes_gemfile?(ENV['TM_FILEPATH'])
     file = File.readlines(ENV['TM_RUBY'])
     ruby_gemset = file.detect { |line| line =~ /source/ }.match(/environments\/([^'"]+)/)[1]
     base_dir = ENV['TM_RUBY'].match(/(.*.rvm\/).*/)[1]
     File.join(base_dir, 'gems', ruby_gemset, 'bin', 'bundle')
   end
-  
-  if File.exists?(bundle_cmd)
+
+  if bundle_cmd and File.exists?(bundle_cmd)
     cmd = [bundle_cmd, 'exec'] + cmd
   end
 end
 
-TextMate::Executor.run(cmd, :version_args => ["--version"], :script_args => script_args) do |str, type|  
+TextMate::Executor.run(cmd, :version_args => ["--version"], :script_args => script_args) do |str, type|
   output = case type
   when :out
     if is_test_script and str =~ /\A[.EF]+\Z/
       format_test_result(htmlize(str)) + "<br style=\"display: none\"/>"
     elsif is_test_script
       out = ""
-      [str].flatten.each do |line|        
+      [str].flatten.each do |line|
         out << if trp.test_result_line?(line)
           trp.process_line(line)
         elsif line =~ /^\d+ tests, \d+ assertions, (\d+) failures, (\d+) errors\b.*/  # test summary
@@ -227,6 +237,6 @@ TextMate::Executor.run(cmd, :version_args => ["--version"], :script_args => scri
       "<span style=\"color: red\">#{htmlize str}</span>"
     end
   end
-  
+
   trp.post_filter(output)
 end
